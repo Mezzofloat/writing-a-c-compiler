4e18d1d8e91ad251c5142cfd369998f1c5444e28
Mezzofloat <skull.wizard.2299@gmail.com>
2026-05-28 09:58:13 -0400

    refactored compiler

--- compiler/compiler.py
+++ compiler/compiler.py
@@ -74,40 +74,33 @@ if args.assembly_lang == "risc-v":
     prefix = "/common/users/shared/cs211_s26_5678/rv32gc-ilp32/bin/riscv32-unknown-linux-gnu-"
     print("compiling for risc-v")
 
-    # generate risc-v ast
-    RISC_ast = risc_converter.attack_to_risc_ast(ATTACK_ast)
-    print("First RISC_ast:")
-    print(RISC_ast)
+    convert = risc_converter.attack_to_risc_ast
+    add_stacks = risc_converter.replace_pseudo_registers
+    fix_insts = risc_converter.fix_instructions
 
-    risc_converter.replace_pseudo_registers(RISC_ast)
-    print("Non-pseudo ast:")
-    print(RISC_ast)
-
-    risc_converter.fix_instructions(RISC_ast)
-    print("Fixed-instructions ast:")
-    print(RISC_ast)
-
-    code = RISC_ast
     emit_code = risc_emitter.emit_risc
 else:
     prefix = ""
     print("compiling for x86")
 
-    # generate assembly ast
-    Assembly_ast = x86_converter.attack_to_x86_ast(ATTACK_ast)
-    print("First Assembly_ast:")
-    print(Assembly_ast)
+    convert = x86_converter.attack_to_x86_ast
+    add_stacks = x86_converter.replace_pseudo_registers
+    fix_insts = x86_converter.fix_instructions
 
-    x86_converter.replace_pseudo_registers(Assembly_ast)
-    print("Non-pseudo ast:")
-    print(Assembly_ast)
-
-    x86_converter.fix_instructions(Assembly_ast)
-    print("Fixed-instructions ast:")
-    print(Assembly_ast)
-
-    code = Assembly_ast
     emit_code = x86_emitter.emit_x86
+
+# generate assembly ast based on whether it's for risc-v or x86
+Assembly_ast = convert(ATTACK_ast)
+print("First Assembly_ast:")
+print(Assembly_ast)
+
+add_stacks(Assembly_ast)
+print("Non-pseudo ast:")
+print(Assembly_ast)
+
+fix_insts(Assembly_ast)
+print("Fixed-instructions ast:")
+print(Assembly_ast)
 
 if args.codegen:
     print('stopping at codegen')
@@ -116,7 +109,7 @@ if args.codegen:
 # emit the code for each node in the tree
 
 with open(assembly, "w") as f:
-    print(emit_code(code), file=f)
+    print(emit_code(Assembly_ast), file=f)
 
 # assemble and link
 os.system(f"{prefix}gcc {assembly} -o {executable}")
