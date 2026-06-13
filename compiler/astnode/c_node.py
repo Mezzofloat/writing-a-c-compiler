7bc245d54e3ab00b208f7de760b78b801d9a8b08
Mezzofloat <skull.wizard.2299@gmail.com>
2026-06-12 23:52:57 -0400

    removed expect function and try except clause from c_node

--- compiler/astnode/c_node.py
+++ compiler/astnode/c_node.py
@@ -58,64 +58,24 @@ class C_node(ASTNode):
     def __init__(self, ident, child=None):
         super().__init__(ident)
         
-        try:
-            LIST = 0
-
-            def expect(*args):
-                if len(args) == 1:
-                    if type(args[0]) is not list or type(child) is not C_node:
-                        raise TypeError(f"Error in function call of expect({args})")
-                    
-                    ls = args[0]
-                    
-                    if child.ident not in ls and child.ident[0] not in ls:
-                        raise AssertionError(f"Error in asserting grammar on {self}")
-                elif len(args) == 2:
-                    if args[0] == LIST:
-                        for entry in child: # type: ignore
-                            if entry.ident not in args[1]:
-                                raise AssertionError(f"Error in asserting grammar on {self}")
-                            
-                        return
-
-                    if type(args[0]) is not str or type(args[1]) is not list or type(child) is not dict:
-                        raise TypeError(f"Error in function call of expect({args})")
-                    
-                    key = args[0]
-                    ls = args[1]
-
+        if ident not in C_node_types:
+            if type(ident) is not tuple:
+                raise ValueError(f"unexpected node {ident}")
+        else:
+            vals = C_node_types[ident]
+            if type(vals) is dict:
+                for key, ls in vals.items():
                     if child[key].ident not in ls and child[key].ident[0] not in ls:
-                        raise AssertionError(f"Error in asserting grammar on {self}")
-                elif len(args) == 0:
+                        raise ValueError(f"{key} should be in {ls} but got {child[key].ident} for node {ident}")
+            elif type(vals) is list:
+                if len(vals) == 0:
                     if child is not None:
-                        raise AssertionError(f"Error in asserting grammar on {self}")
+                        raise ValueError(f"unexpected child {child} for node {ident}")
                 else:
-                    raise SyntaxError("Incorrect number of arguments")
-
-            if ident not in C_node_types:
-                if type(ident) is tuple:
-                    pass
-                else:
-                    raise ValueError(f"unexpected node {ident}")
+                    if child.ident not in vals and child.ident[0] not in vals:
+                        raise ValueError(f"unexpected child {child} for node {ident}")
             else:
-                vals = C_node_types[ident]
-                if type(vals) is dict:
-                    for key, ls in vals.items():
-                        expect(key, ls)
-                elif type(vals) is list:
-                    if len(vals) == 0:
-                        expect()
-                    else:
-                        expect(vals)
-                else:
-                    raise TypeError(f"unexpected type of node {ident} in C_node_types")
+                raise TypeError(f"unexpected type of node {ident} in C_node_types")
 
-        except AttributeError:
-            raise AssertionError(f"Error in accessing attribute; {child} is either None or wrong type")
-        except KeyError:
-            raise AssertionError(f"Error in accessing dict; {child} is either not dict or doesn't have required key")
-        except IndexError:
-            raise AssertionError(f"Error in indexing tuple; {child} doesn't have a tuple somewhere")
-        finally:
-            self.child = child
+        self.child = child
         
