2b502cf8dec0f9bbc5b2d24d49d14663d9b8d8ae
Mezzofloat <skull.wizard.2299@gmail.com>
2026-06-16 12:16:20 -0400

    added variable resolution semantic analysis

--- compiler/compiler.py
+++ compiler/compiler.py
@@ -5,6 +5,7 @@ import os
 # time how long each section takes
 import time
 
+from astnode.c_node import C_node
 from lexer import lex
 from parser import parse_program
 from attack_code_generator import c_to_attack
@@ -72,6 +73,86 @@ if args.time:
 if args.parse:
     print('stopping at parse')
     exit(0)
+
+temp_idx = 0
+def get_temp(initial):
+    global temp_idx
+    unique = f"{initial}_{temp_idx}"
+    temp_idx += 1
+    return unique
+
+variable_map = {}
+def resolve_declaration(node):
+    name = node.child["name"].ident[1]
+    if name in variable_map:
+        raise RuntimeError(f"Variable {name} already declared")
+    unique = get_temp(name)
+    variable_map[name] = unique
+
+    init = node.child["init"]
+    newinit = None
+    if init is not None:
+        newinit = resolve_exp(init)
+
+    return C_node("Declaration", {
+        "name": C_node(("Identifier", unique)),
+        "init": newinit
+    })
+
+def resolve_statement(node):
+    statement = node.child
+    if statement.ident == "Return":
+        exp = resolve_exp(statement.child)
+        return C_node("Statement", C_node("Return", exp))
+    elif statement.ident == "Null":
+        return node
+    else:
+        exp = resolve_exp(statement)
+        return C_node("Statement", exp)
+
+def resolve_exp(node):
+    match node.ident:
+        case "Assignment":
+            if node.child["lvalue"].ident != "Var":
+                raise RuntimeError("Left-hand side of assignment must be a variable")
+            return C_node("Assignment", {
+                "lvalue": resolve_exp(node.child["lvalue"]),
+                "exp": resolve_exp(node.child["exp"])
+            })
+        case "Var":
+            if node.child.ident[1] not in variable_map:
+                raise RuntimeError(f"Variable {node.child.ident[1]} not declared")
+            return C_node("Var", C_node(("Identifier", variable_map[node.child.ident[1]])))
+        case "Unary":
+            return C_node("Unary", {
+                "op": node.child["op"],
+                "inner_exp": resolve_exp(node.child["inner_exp"])
+            })
+        case "Binary":
+            return C_node("Binary", {
+                "op": node.child["op"],
+                "left": resolve_exp(node.child["left"]),
+                "right": resolve_exp(node.child["right"])
+            })
+        case _:
+            return node
+        
+func = C_ast.child
+insts = []
+for block_item in func.child["body"]:
+    if block_item.child.ident == "Declaration":
+        decl = resolve_declaration(block_item.child)
+
+        insts.append(C_node("BlockItem", decl))
+    elif block_item.child.ident == "Statement":
+        stat = resolve_statement(block_item.child)
+
+        insts.append(C_node("BlockItem", stat))
+
+var_resolved_ast = C_node("Program", C_node("Function", {"name": func.child["name"], "body": insts}))
+
+print("Variable resolution:")
+print(var_resolved_ast)
 
 if args.validate:
     print('stopping at semantic validation')
